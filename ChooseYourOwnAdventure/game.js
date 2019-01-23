bdfe96c5b77194d73c9b3d8333cc4b71b487ab87
Unknown <porter.g829@stu.nebo.edu>
2019-01-23 10:27:30 -0700

    worked a bit

---
+++ ChooseYourOwnAdventure/game.js
@@ -0,0 +1,20 @@
+// Single line comment
+/*
+
+Multiline comment
+
+*/
+
+//document.write("I would just like to learn something");
+
+//alert("Alarm.  Wake up.  This is a test.");
+
+//confirm("Do you like Melee");
+var xPosition = 3;
+var yPosition = 1;
+
+var playerName = prompt("What is your name my boi?");
+while (playerName == null)
+    {
+        playerName = prompt("You didn't enter something you dumb.  What is your name?");
+    }
